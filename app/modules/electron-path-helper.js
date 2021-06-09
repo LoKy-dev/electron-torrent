42ae87518d38567d9aef9c8960e87cb5c99a756b
Lorenzo Pecchio <lorenzopecchio.developer@gmail.com>
2021-06-10 00:50:22 +0200

    Create first working react app with boilerplate code

--- app/modules/electron-path-helper.js
+++
@@ -1,6 +0,0 @@
-const path = require('path')
-
-exports.getHtmlAbsolutePath = (filename) => {
-  // return `app/renderer/html/${filename}.html`;
-  return path.join('app/renderer/html', `${filename}.html`);
-}
